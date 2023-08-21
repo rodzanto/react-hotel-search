9092d062028028202fb81adc3ba5eb4107d831c2
Joseba Echevarria García <joseba.gar@gmail.com>
2023-08-21 17:24:50 +0200

    Remove old code until we get the few-shot code working again

--- src/app.py
+++ src/app.py
@@ -78,20 +78,11 @@ def main():
     rds_uri = get_rds_uri(REGION_NAME)
     db = SQLDatabase.from_uri(rds_uri)
 
-    # load examples for few-shot prompting
-    examples = load_samples()
-
     # Create the LangChain agent and equip it with a toolkit
     llm = Bedrock(model_id=MODEL_NAME,
                   client=bedrock_client,
                   model_kwargs=inference_params)
     toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-    sql_db_chain = load_few_shot_chain(llm, db, examples)
-    tools = load_tools(["llm-math"], llm=llm)
-    sql_tool = Tool(name='Hotels DB', func=sql_db_chain.run,
-                    description="Useful for when you need to answer questions about hotels and their ratings.")
-
-    tools.append(sql_tool)
 
     agent_executor = create_sql_agent(llm=llm,
                                       toolkit=toolkit,
@@ -101,6 +92,20 @@ def main():
                                                              'return_intermediate_steps': True},
                                       verbose=True,
                                       early_stopping_method='generate',
+                                      prefix='''Assistant is a large language model trained by Amazon.
+
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+Assistant has access to a {dialect} database whose main table name is wb_hotels that contains information about hotels in different cities, Assistant can query it to get details about hotels.
+
+TOOLS:
+------
+Assistant has access to the following tools:
+''',
                                       format_instructions='''To use a tool, please use the following format:
 
     ```
@@ -115,21 +120,7 @@ def main():
     ```
     Thought: Do I need to use a tool? No
     {ai_prefix}: [your response here]
-    ```''',
-                                      prefix='''Assistant is a large language model trained by Amazon.
-
-                                          Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-
-                                          Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-
-                                          Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-
-                                          Assistant has access to a {dialect} database whose main table name is wb_hotels that contains information about hotels in different cities, Assistant can query it to get details about hotels.
-
-                                          TOOLS:
-                                          ------
-                                          Assistant has access to the following tools:
-                                          ''')
+    ```''')
 
     # store the initial value of widgets in session state
     if "visibility" not in st.session_state:
