cc8cce0c067064043840d34f21339b03dc569167
Joseba Echevarria García <joseba.gar@gmail.com>
2023-08-22 11:04:04 +0200

    Removed testing code

--- src/agents/webbeds.py
+++ src/agents/webbeds.py
@@ -2,9 +2,8 @@
 from typing import Any, Dict, Optional
 
 from langchain.agents.agent import AgentExecutor
-from langchain.agents.conversational.prompt import (PREFIX, FORMAT_INSTRUCTIONS, SUFFIX)
-from langchain.agents.agent_toolkits.sql.prompt import (SQL_PREFIX,
-                                                        SQL_SUFFIX)
+from langchain.agents.conversational.prompt import FORMAT_INSTRUCTIONS, SUFFIX
+from langchain.agents.agent_toolkits.sql.prompt import SQL_PREFIX
 from langchain.agents.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain.callbacks.base import BaseCallbackManager
 from langchain.schema.language_model import BaseLanguageModel
@@ -44,55 +43,3 @@ def create_sql_agent(llm: BaseLanguageModel,
                                               max_execution_time=max_execution_time,
                                               early_stopping_method=early_stopping_method,
                                               **(agent_executor_kwargs or {}))
-
-
-if __name__ == '__main__':
-    from langchain.sql_database import SQLDatabase
-    from langchain.llms.bedrock import Bedrock
-    from langchain.memory import ConversationBufferMemory
-
-    llm = Bedrock(model_id='anthropic.claude-v2',
-                  model_kwargs={'max_tokens_to_sample': 4096,
-                                'temperature': 0.5,
-                                'top_k': 250,
-                                'stop_sequences': ['\n\nQuestion'],
-                                'top_p': 1})
-    db = SQLDatabase.from_uri('postgresql+psycopg2://postgres:wdbQDPDKGYHwpZdVJ4Jr@localhost:5432/wb_hotels')
-    toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-    memory = ConversationBufferMemory(memory_key="chat_history")
-    agent_executor = create_sql_agent(llm=llm,
-                                      toolkit=toolkit,
-                                      agent_executor_kwargs={'memory': memory},
-                                      verbose=True,
-                                      format_instructions='''To use a tool, please use the following format:
-
-    ```
-    Thought: Do I need to use a tool? Yes
-    Action: the action to take, should be one of [{tool_names}]
-    Action Input: the input to the action
-    Observation: the result of the action
-    ```
-
-    When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
-
-    ```
-    Thought: Do I need to use a tool? No
-    {ai_prefix}: [your response here]
-    ```''',
-                                      prefix='''Assistant is a large language model trained by Amazon.
-
-                                      Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-
-                                      Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-
-                                      Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-
-                                      Assistant has access to a {dialect} database whose main table name is wb_hotels that contains information about hotels in different cities, Assistant can query it to get details about hotels.
-
-                                      TOOLS:
-                                      ------
-                                      Assistant has access to the following tools:
-                                      ''')
-
-    agent_executor.run(input='How many hotels are there?')
-    agent_executor.run(input="How many of those are in CADIZ?")
